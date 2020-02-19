e9d177fecc4e8646ff79c726a232bed80303fdf7
Dylan <dylan.bouchet@yahoo.fr>
2020-02-19 17:19:36 +0100

    petites modifs + mise à jour npm + essais de boites modales.

--- AngularView/src/assets/js/script.js
+++ AngularView/src/assets/js/script.js
@@ -1,29 +1,45 @@
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox({
-    alwaysShowClose: true,
-    alwaysShowArrows: true,
-    leftArrow: "GAUCHE",
-    rightArrow: ">",
+// $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+//   event.preventDefault();
+//   $(this).ekkoLightbox({
+//     alwaysShowClose: true,
+//     alwaysShowArrows: true,
+//     leftArrow: "GAUCHE",
+//     rightArrow: ">",
 
-    onShown: function() {
-      console.log("Checking our the events huh?");
-    }
+//     onShown: function() {
+//       console.log("Checking our the events huh?");
+//     }
+//   });
+// });
+// var lightbox = new SimpleLightbox(".gallery a", {
+//   /* options */
+// });
+
+$(document).ready(function() {
+  $("[data-fancybox]").fancybox({
+    protect: true
   });
 });
 
 function pageRedirect() {
   window.location.replace("http://localhost:4200/home");
 }
+
 $(document).on("click", ".super-button", function() {
-  setTimeout("pageRedirect()", 5000);
+  setTimeout("pageRedirect()", 3000);
 });
 
 $(document).ready(function() {
   $(document).on("click", ".super-button", function() {
     $(".main, .half, .overlay, .button-line, .super-button").addClass("active");
     setTimeout(function() {
-      $(".main *").css( "z-index", "1");
+      $(".main *").css("z-index", "1");
     }, 2800);
   });
+});
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".loading").css("z-index", "1");
+  }, 1500);
 });
